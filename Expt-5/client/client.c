36d249df8f49a44e2612c61993c050e0553658dc
Archi Halder <archihalder23026558@gmail.com>
2021-09-15 17:35:18 +0530

    added client file

---
+++ Expt-5/client/client.c
@@ -0,0 +1,36 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+int main()
+{
+    struct sockaddr_in sa, cli;
+    int n, sockfd;
+    int len;
+    char buff[100];
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        printf("\nError in Socket");
+        exit(0);
+    }
+    bzero(&sa, sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(1848);
+    if (connect(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
+    {
+        printf("\nError in connection failed\n");
+        exit(0);
+    }
+    if (n = read(sockfd, buff, sizeof(buff)) < 0)
+    {
+        printf("\nError in Reading");
+        exit(0);
+    }
+    else
+    {
+        printf("\nDay and Time of Server is %s", buff);
+    }
+}
